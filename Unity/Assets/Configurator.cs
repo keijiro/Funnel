0479356c5d05621dce8eeffd2cf663449f45fef3
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-12-29 16:27:03 +0900

    Tweaked the test project.

---
+++ Unity/Assets/Configurator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Configurator : MonoBehaviour
+{
+    public int framerate = 60;
+    public bool fixFramerate = false;
+
+    void Start ()
+    {
+        if (fixFramerate) {
+            Time.captureFramerate = framerate;
+        } else {
+            Application.targetFrameRate = framerate;
+        }
+    }
+}
